e5c9023396b7fd4179527a7fbf8601133d48dbd0
miwanoff <marina.iva.noffa@gmail.com>
2023-12-02 15:33:53 +0200

    Add imagesetthickness

--- image4.php
+++ image4.php
@@ -20,19 +20,30 @@ $canvas = imagecreatetruecolor(200, 200);
 $pink = imagecolorallocate($canvas, 255, 105, 180);
 $white = imagecolorallocate($canvas, 255, 255, 255);
 $green = imagecolorallocate($canvas, 132, 135, 28);
-
+$blue = imagecolorallocate($canvas, 0, 0, 255);
+$red = imagecolorallocate($canvas, 255, 0, 0);
+imagesetthickness ( $canvas, 5 );
 // Рисование разноцветных прямоугольников
 imagerectangle($canvas, 50, 50, 150, 150, $pink);
-imagerectangle($canvas, 45, 60, 120, 100, $white);
+imagefilledrectangle($canvas, 45, 60, 120, 100, $white);
 imagerectangle($canvas, 100, 120, 75, 160, $green);
 
 imageline($canvas, 0, 0, imageSX($canvas) - 1, imageSY($canvas) - 1, $green);
-imagepolygon($canvas, [
+imagefilledpolygon($canvas, [
     0, 0,
     120, 100,
     150, 200,
 ],
     $pink);
+// рисуем голову
+imagefilledarc($canvas, 100, 100, 200, 200, 0, 360, $white, IMG_ARC_EDGED );
+
+// рот
+imagearc($canvas, 100, 100, 150, 150, 25, 155, $red);
+
+// глаза
+imagearc($canvas, 60, 75, 50, 50, 0, 360, $green);
+imagearc($canvas, 140, 75, 50, 50, 0, 360, $blue);
 // Вывод и освобождение памяти
 header('Content-Type: image/jpeg');
 imagejpeg($canvas);
